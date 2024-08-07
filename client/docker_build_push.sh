d82f76f6da5409b26d2276d031287b748ce92976
Kartikey Tiwari <123266503+Codewallahh@users.noreply.github.com>
2024-08-07 22:47:25 +0530

    Update and rename Makefile to docker_build_push.sh

---
+++ client/docker_build_push.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+# Define variables
+TAG=${TAG:-0}
+IMAGE_NAME=ci-cd-mern-dc-frontend
+REPOSITORY=tiwari123
+FULL_TAG=${REPOSITORY}/${IMAGE_NAME}:v1.0.${TAG}
+
+# Docker login
+echo "Logging into Docker Hub..."
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build --platform linux/amd64 -t ${IMAGE_NAME}:v1.0.${TAG} .
+
+# Tag and push the Docker image
+echo "Pushing Docker image to Docker Hub..."
+docker tag ${IMAGE_NAME}:v1.0.${TAG} ${FULL_TAG}
+docker push ${FULL_TAG}
+
+echo "Docker image pushed successfully: ${FULL_TAG}"
+
