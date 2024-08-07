72aa40962d4c06955f8a066daf27bbc7011ceb16
Kartikey Tiwari <123266503+Codewallahh@users.noreply.github.com>
2024-08-07 23:04:31 +0530

    Update docker_build_push.sh

--- client/docker_build_push.sh
+++ client/docker_build_push.sh
@@ -1,24 +1,12 @@
-
 #!/bin/bash
 
-# Define variables
-TAG=${TAG:-0}
-IMAGE_NAME=ci-cd-mern-dc-frontend
-REPOSITORY=tiwari123
-FULL_TAG=${REPOSITORY}/${IMAGE_NAME}:v1.0.${TAG}
-
-# Docker login
-echo "Logging into Docker Hub..."
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+TAG=$1  # Capture the first argument passed to the script
+tag=v1.$TAG
+registry=tiwari123
+repository=3-tier-frontend
 
 # Build the Docker image
-echo "Building Docker image..."
-docker build --platform linux/amd64 -t ${IMAGE_NAME}:v1.0.${TAG} .
+docker build -t $registry/$repository:$tag .
 
-# Tag and push the Docker image
-echo "Pushing Docker image to Docker Hub..."
-docker tag ${IMAGE_NAME}:v1.0.${TAG} ${FULL_TAG}
-docker push ${FULL_TAG}
-
-echo "Docker image pushed successfully: ${FULL_TAG}"
-
+# Push the Docker image to the registry
+docker push $registry/$repository:$tag
